cd391160ae1f14521f4d3520798fd855b24a7d2a
BGMarinho <bgmarinho96@gmail.com>
2024-06-19 01:00:57 -0300

    Adding an example using the Promise constructor to cli.js notes!

--- src/cli.js
+++ src/cli.js
@@ -137,3 +137,22 @@ function criaESalvaArquivos(listaPalavras, endereco) {
    * em implementações mais recentes.
    */
 }
+
+// Exemplo de uso do construtor "new Promise(callback)"
+// function promessa(bool) {
+//   const x = bool;
+//   return new Promise((resolve, reject) => {
+//     if (!x) {
+//       reject(new Error("falha na promessa"));
+//     }
+//     resolve("sucesso na promessa");
+//   });
+//  }
+
+//  function exibeResposta(textoResult) {
+//   console.log(textoResult);
+//  }
+
+//  promessa(true)
+//   .then((texto) => exibeResposta(texto))
+//  // sucesso na promessa
